53451572b162e992a3bdf4238f48f714fc60c257
Sshivaniiii <Uprarishivani@gmail.com>
2024-07-12 20:59:48 +0530

    Add files via upload

---
+++ stars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, k, row;
+    cout << "enter value of row";
+    cin >> row;
+    for (i = 1; i <= row; i++){
+        for (j = 1; j <= row - i; j++){
+            cout << " ";
+        }
+        for (k = 1; k <= 2 * i - 1; k++){
+            cout << "*";
+        }
+    cout<<endl;
+    }
+
+}
