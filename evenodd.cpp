140a85d2b5fcf547eb78cbc96637983030ecf490
Sshivaniiii <Uprarishivani@gmail.com>
2024-07-14 20:44:31 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter value of num";
+    cin>>num;
+    if(num%2==0){
+        cout<<"it is even";
+    } else{
+        cout<<"it is odd";
+    }
+}
