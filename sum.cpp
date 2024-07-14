87182a7d363e3cebe3c64fa91b3a61af85465d7e
Sshivaniiii <Uprarishivani@gmail.com>
2024-07-14 22:38:41 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+    int stay(int a,int b ){
+    return a+b;
+    }
+    int main(){
+    int num1,num2,sum;
+cout<<"enter num1"<<endl;
+cout<<"enter num2"<<endl;
+cin>>num1;
+cin>>num2;
+ sum=stay(num1,num2);
+ cout<<sum;
+
+
+}
