6e85912696ad19c437e07e8fa8017320006e4fb2
Sshivaniiii <Uprarishivani@gmail.com>
2024-07-18 18:50:46 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int  all[10]={1,3,56,78,99,98,78,95,67,35};
+    cout<<"the numbers are"<<endl;
+    for( int n:all){
+        cout<<n<<" ";
+    }
+
+}
