c22259f70f5183c6bcc03665e69754b04ea500fc
Sshivaniiii <Uprarishivani@gmail.com>
2024-07-30 23:58:08 +0530

    Add files via upload

---
+++ arrayfill.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a[5];
+    int i;
+    for(i=0;i<=4;i++){
+        cout<<"enter the elements"<<endl;
+        cin>>a[i];
+}
+for(i=0;i<=4;i++){
+    cout<<a[i]<<" ";
+}
+}
